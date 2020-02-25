1a1f68681422816ca501e3ab911dd31e6f816f2a
0xbrendan <b.b@b.com>
2020-02-25 22:54:33 +0000

    filled in secure ssh script

--- sys265/linux/centos7/secure-ssh.sh
+++ sys265/linux/centos7/secure-ssh.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 #secure-ssh.sh
 #author brendan
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
+USERN=$1
+
+sudo useradd -m -d /home/$USERN -s /bin/bash $USERN
+sudo mkdir /home/$USERN/.ssh
+sudo cp ../../../sys265/linux/public-keys/id_rsa.pub /home/$USERN/.ssh/authorized_keys
+sudo chmod 700 /home/$USERN/.ssh
+sudo chmod 600 /home/$USERN/.ssh/authorized_keys
+sudo chown -R $USERN:$USERN /home/$USERN/.ssh
+
